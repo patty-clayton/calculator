f03086233b775509b3403d830336797de1ce87f2
patty-clayton <hello@patty-clayton.com>
2020-06-11 17:12:29 -0700

    added 3 test cases

--- test/example.test.js
+++ test/example.test.js
@@ -22,6 +22,8 @@ test('time to test a function', function(expect) {
 //view the test/index.html in live to see the test results!
 //when testing site, its always helpful to keep your inspect>console window open!
 //
+//
+//test addition x3
 test('addition', function(expect) {
     const firstnum = 2;
     const secondnum = 4;
@@ -29,6 +31,23 @@ test('addition', function(expect) {
     const actual = addition(firstnum, secondnum);
     expect.equal(expected, actual);
 });
+test('addition', function(expect) {
+    const firstnum = 0.25;
+    const secondnum = 4;
+    const expected = 4.25;
+    const actual = addition(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+test('addition', function(expect) {
+    const firstnum = -2;
+    const secondnum = 4;
+    const expected = 2;
+    const actual = addition(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+//
+//
+//test subtraction x3
 test('subtraction', function(expect) {
     const firstnum = 2;
     const secondnum = 4;
@@ -36,6 +55,23 @@ test('subtraction', function(expect) {
     const actual = subtraction(firstnum, secondnum);
     expect.equal(expected, actual);
 });
+test('subtraction', function(expect) {
+    const firstnum = 2;
+    const secondnum = -4;
+    const expected = 6;
+    const actual = subtraction(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+test('subtraction', function(expect) {
+    const firstnum = 2.1;
+    const secondnum = 0.1;
+    const expected = 2;
+    const actual = subtraction(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+//
+//
+//test multiply x3
 test('multiply', function(expect) {
     const firstnum = 2;
     const secondnum = 4;
@@ -43,10 +79,41 @@ test('multiply', function(expect) {
     const actual = multiply(firstnum, secondnum);
     expect.equal(expected, actual);
 });
+test('multiply', function(expect) {
+    const firstnum = 0.5;
+    const secondnum = 0.5;
+    const expected = 0.25;
+    const actual = multiply(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+test('multiply', function(expect) {
+    const firstnum = 2;
+    const secondnum = 0;
+    const expected = 0;
+    const actual = multiply(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+//
+//
+//test division x3
 test('divide', function(expect) {
     const firstnum = 6;
     const secondnum = 2;
     const expected = 3;
+    const actual = divide(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+test('divide', function(expect) {
+    const firstnum = 0;
+    const secondnum = 2;
+    const expected = 0;
+    const actual = divide(firstnum, secondnum);
+    expect.equal(expected, actual);
+});
+test('divide', function(expect) {
+    const firstnum = 0.2;
+    const secondnum = 1;
+    const expected = 0.2;
     const actual = divide(firstnum, secondnum);
     expect.equal(expected, actual);
 });
